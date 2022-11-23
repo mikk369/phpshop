310c16aaa4280a0da20d70fc5405f8c3907132c4
mikk369 <mikk.ereline@vikk.ee>
2022-11-23 22:07:48 +0200

    changed db credentials

--- dbConn.php
+++ dbConn.php
@@ -1,5 +1,5 @@
 <?php
 $serverName = "localhost";
-$userName = "root";
-$password = "";
-$database = "webshop";
+$userName = "id19902331_phpshop";
+$password = "db2P2N3QXMJM5y%l";
+$database = "id19902331_webshop";
