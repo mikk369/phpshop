1860aec85591954f8c758ec3be973bcf97e36861
mikk369 <mikk.ereline@vikk.ee>
2022-11-27 12:06:50 +0200

    connection

--- dbConn.php
+++ dbConn.php
@@ -1,5 +1,5 @@
 <?php
 $serverName = "localhost";
-$userName = "id19902331_phpshop";
-$password = "db2P2N3QXMJM5y%l";
-$database = "id19902331_webshop";
+$userName = "root";
+$password = "";
+$database = "webshop";
